e913ba2cd1e485dd20a42d927d5114e2de41edc3
ujangdoubleday <domba88galing@gmail.com>
2025-12-01 13:00:41 +0700

    chore: made main() synchronous

--- crates/app/src/main.rs
+++ crates/app/src/main.rs
@@ -3,13 +3,13 @@ use std::env;
 
 use x_cli::Cli;
 
-#[tokio::main]
-async fn main() -> anyhow::Result<()> {
+fn main() -> anyhow::Result<()> {
     let args: Vec<String> = env::args().collect();
 
     if args.len() > 1 {
+        let rt = tokio::runtime::Runtime::new()?;
         let cli = Cli::parse();
-        cli.execute().await
+        rt.block_on(cli.execute())
     } else {
         x_cli::ui::run()
     }
